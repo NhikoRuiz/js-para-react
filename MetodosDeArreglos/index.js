e7e8a95b0a3ba7b99a8cd555006f8cbe686bf959
Nicolás <nicolaskruiz@gmail.com>
2023-05-27 00:24:05 +0100

    Metodos de arreglos(arrays)

---
+++ MetodosDeArreglos/index.js
@@ -0,0 +1,53 @@
+// // arrays methods
+
+const names = ['ryan','joe','maria'];
+
+// FOREACH
+// names.forEach((name)=> console.log(name))
+
+
+// MAP
+
+// const newNames = names.map(function(name){
+//     return `Hola ${name}`
+// })
+
+// console.log(newNames)
+// console.log(names)
+
+
+
+
+
+// FIND = Busca
+
+// const nameFound = names.find(function(name){
+//     if (name === 'joe'){
+//         return name
+//     }
+// })
+
+// console.log(nameFound)
+
+
+
+// FILTER = crea nuevos arreglos con elementos flitrados
+
+// const newName = names.filter(function(name){
+//     if (name === 'joe'){
+//         return name
+//     }
+// })
+
+// console.log(newName)
+
+
+// CONCAT
+
+const newNames = ['marcos','mario','john'];
+console.log(names.concat(newNames))
+
+
+
+
+
